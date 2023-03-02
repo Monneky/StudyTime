cf7dbdbe733d4f7e8d7402bb3af5303e04260e69
Cesar Ivann Llamas <ivllamoficial@gmail.com>
2023-03-02 16:33:55 -0600

    we create a pop up that help us to explain all our app

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flet import Page,Text, NavigationBar, NavigationDestination, icons,app, FontWeight, FloatingActionButton
+from flet import Page, Text, NavigationBar, NavigationDestination, icons,app, FontWeight, FloatingActionButton, AlertDialog
 
 if __name__ == "__main__":
     def main(page: Page):
-        def open_info(e):
-            page.dialog = "Hello there"
-            
         page.title = "Home"
+        
+        dlg = AlertDialog(
+            title = Text("Hello, you!"), on_dismiss=lambda e: print("Dialog dismissed!")
+        )
+
+        def open_dlg(e):
+            page.dialog = dlg
+            dlg.open = True
+            page.update()
+
         page.navigation_bar = NavigationBar(
             destinations= [
                 NavigationDestination(icon=icons.PLAY_ARROW, label="Start"),
@@ -15,12 +22,11 @@ if __name__ == "__main__":
         )
         page.floating_action_button = FloatingActionButton(
             icon=icons.HELP,
-            on_click=open_info
+            on_click=open_dlg
         )
         page.add(
             Text("Welcome to the Study Timer", size=50,  weight=FontWeight.W_900),
-            Text("In the below menu you can choose an option, if you have doubts please click in the help icon to see how to use this app, thank you.", size=20),
-            Text(f'Route {page.route}')
+            Text("In the below menu you can choose an option, if you have doubts please click in the help icon to see how to use this app, thank you.", size=20)
         )
 
 app(target=main)
